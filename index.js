f761181693733e74e96a513696a372393427b91f
wagneralmeida-liveonsolutions <wagner.almeida@liveonsolutions.com>
2023-03-21 13:46:02 -0300

    format prisma

---
+++ index.js
@@ -0,0 +1,17 @@
+const test = {
+  firstName: 'Wagner',
+  lastName: 'Almeida',
+  age: 18,
+  address: '',
+  number: ''
+}
+
+const ar = ['']
+
+ar.map(item, index => item)
+
+export default function (first, second, thirth) {
+  return ''
+}
+
+const fun = () => {}
